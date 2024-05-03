2c31aceb0029f6666b38f7fe3d337c7b84573490
deepanshkhurana <deepanshkhurana@outlook.com>
2024-05-04 04:04:39 +0530

    feat: add selected app and table to shiny inputs

--- app/js/index.js
+++ app/js/index.js
@@ -1,15 +1,30 @@
 export function toggleIcon() {
   $('.argus-icon').click((evt) => {
     const target = evt.currentTarget;
+    const selectedApp = $('#app-selector-application').val();
+    const selectedTable = $('#app-selector-table').val();
 
     // This condition is reversed becuase if the eye has a class, that means
     // it will be toggled. In other words, hasClass tells wasClass
 
     const mode = $(target).hasClass('fa-eye') ? 'add' : 'view';
     $(target).toggleClass('fa-eye fa-eye-dropper');
+
     Shiny.setInputValue(
       'app-app_mode',
       mode,
+      { priority: 'event' },
+    );
+
+    Shiny.setInputValue(
+      'app-selected_app',
+      selectedApp,
+      { priority: 'event' },
+    );
+
+    Shiny.setInputValue(
+      'app-selected_table',
+      selectedTable,
       { priority: 'event' },
     );
   });
